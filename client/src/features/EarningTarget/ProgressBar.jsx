87795d696bd6b9c751697370aa95fd298173cd11
rzngnam1402 <legiangnam1402@gmail.com>
2023-12-18 14:37:19 +0700

    fix: ProgressBar

--- client/src/features/EarningTarget/ProgressBar.jsx
+++ client/src/features/EarningTarget/ProgressBar.jsx
@@ -1,40 +1,39 @@
-import React from "react";
+import React, { useState } from 'react';
 
 const ProgressBar = (props) => {
-    const { bgcolor, completed } = props;
-  
-    const containerStyles = {
-      display: 'table',
-      height: 15,
-      width: '100%',
-      backgroundColor: "#e0e0de",
-      borderRadius: 50,
-      margin: 5
-    }
-  
-    const fillerStyles = {
-      display: 'table',
-      height: '100%',
-      width: `${completed}%`,
-      backgroundColor: bgcolor,
-      borderRadius: 'inherit',
-      textAlign: 'right'
-    }
-  
-    const labelStyles = {
-      marginRight: 20,
-      color: 'white',
-      fontWeight: 'bold',
-      //height: '80%'
-    }
-  
-    return (
-      <div style={containerStyles}>
-        <div style={fillerStyles}>
-          <span style={labelStyles}>{`${completed}%`}</span>
-        </div>
-      </div>
-    );
+  const { bgcolor, completed } = props;
+
+  const containerStyles = {
+    display: 'table',
+    height: 15,
+    width: '100%',
+    backgroundColor: '#e0e0de',
+    borderRadius: 50,
+    margin: 5,
   };
-  
-  export default ProgressBar;
+
+  const fillerStyles = {
+    display: 'table',
+    height: '100%',
+    width: `${completed}%`,
+    backgroundColor: bgcolor,
+    borderRadius: 'inherit',
+    textAlign: 'center',
+    justifyContent: 'center',
+  };
+
+  const labelStyles = {
+    color: 'white',
+    fontWeight: 'bold',
+  };
+
+  return (
+    <div style={containerStyles}>
+      <div style={fillerStyles}>
+        <span style={labelStyles}>{`${completed}%`}</span>
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
